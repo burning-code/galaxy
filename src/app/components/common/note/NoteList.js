fb29772c75aae43b92a22339a7f745bf8da8a4bb
Jared Li <jared.cunrui.li@gmail.com>
2018-09-16 23:04:29 +1200

    remove unused import and variable

--- src/app/components/common/note/NoteList.js
+++ src/app/components/common/note/NoteList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { moment } from '../../../core'
 import { NoteBL } from '../../../helpers'
 import defaultAvatar from '../../../../skin/image/default_avatar.png'
 
